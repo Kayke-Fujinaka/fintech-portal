1f75a5d1c7e04ecffad5c81d5ab4709555cdeba1
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-09 22:25:10 -0300

    chore: add husky and commitlint

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,6 +1,6 @@
 const ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT;
 
 export const baseURL =
-  ENVIRONMENT === "production"
-    ? "https://backend.fintech.fiap.com"
-    : "http://localhost:8085/";
+  ENVIRONMENT === 'production'
+    ? 'https://backend.fintech.fiap.com'
+    : 'http://localhost:8085/';
