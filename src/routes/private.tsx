fcae1bd5a9374a2a327effee7fe68306d275ae28
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-11 21:28:37 -0300

    feat: create interface to route props

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -1,6 +1,8 @@
 import { Navigate } from 'react-router-dom';
 
-export const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+import { IRouteProps } from '../interfaces/routes';
+
+export const PrivateRoute = ({ children }: IRouteProps) => {
   const hasToken = sessionStorage.getItem('@token');
 
   return hasToken ? <>{children}</> : <Navigate to="/login" />;
--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,6 +1,8 @@
 import { Navigate } from 'react-router-dom';
 
-export const PublicRoute = ({ children }: { children: JSX.Element }) => {
+import { IRouteProps } from '../interfaces/routes';
+
+export const PublicRoute = ({ children }: IRouteProps) => {
   const hasToken = sessionStorage.getItem('@token');
 
   return !hasToken ? <>{children}</> : <Navigate to="/investimentos" />;
