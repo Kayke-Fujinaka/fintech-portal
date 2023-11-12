fdd5b8b9f0e9af87aa1a97452d7f280e44334186
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-12 15:17:43 -0300

    chore: add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { router } from './routes';
+import { GlobalStyle } from './styles/global';
+import { theme } from './styles/theme';
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+        <GlobalStyle />
+      </ThemeProvider>
     </>
   );
 }
