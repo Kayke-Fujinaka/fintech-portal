5480b94b601c6f6751239e4b38b063a986ee10ec
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-12 15:38:17 -0300

    feat: add helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
@@ -8,6 +9,22 @@ import { theme } from './styles/theme';
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Fintech</title>
+        <link rel="icon" href={'...'} />
+        <meta property="og:url" content="http://localhost:3001" />
+        <meta property="og:type" content="website" />
+        <meta name="robots" content="all" />
+        <meta name="author" content="Fintech" />
+        <meta
+          name="description"
+          content="Gerencie suas finanças com facilidade e clareza com Fintech. Nossa plataforma ajuda você a controlar suas receitas, gastos, investimentos e dívidas, tudo em um só lugar. Defina metas financeiras, acompanhe seu progresso e tome decisões informadas para um futuro financeiro mais seguro."
+        />
+        <meta
+          name="keywords"
+          content="fintech, gestão financeira, controle de gastos, planejamento financeiro, metas financeiras, orçamento pessoal, saúde financeira, economia, investimentos, finanças pessoais, app de finanças, educação financeira, análise financeira, app de orçamento"
+        />
+      </Helmet>
       <ThemeProvider theme={theme}>
         <RouterProvider router={router} />
         <GlobalStyle />
