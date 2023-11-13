0bd01fe5e2b576253401f341e13e7c35395db811
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-13 08:27:10 -0300

    feat: add logout button

--- src/pages/Portal/Dashboard/index.tsx
+++ src/pages/Portal/Dashboard/index.tsx
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('@token');
+    navigate('/login');
+  };
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <button onClick={handleLogout}>Sair</button>
+    </div>
+  );
 };
 
 export default Dashboard;
