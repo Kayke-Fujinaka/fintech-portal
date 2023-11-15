cb51e599fe54217438575d8ed7cb1fe6f0ca4a47
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-15 16:19:25 -0300

    feat: create themes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,1 +1,27 @@
-export const theme = {};
+export const theme = {
+  fontSizes: {
+    md: '1rem',
+    xl: '1.25rem',
+    '4xl': '2rem',
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+  },
+  colors: {
+    primary: '#ef5933',
+    secondary: '#1e232c',
+    danger: '#f75a68',
+    white: '#ffffff',
+    alpha: {
+      50: '#f7f8f9',
+      300: '#dadada',
+      500: '#6a707c',
+    },
+    grey: {
+      450: '#8391a1',
+    },
+  },
+};
