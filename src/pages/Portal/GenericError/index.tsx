d467940ca886615d4d98895f253877ed57392651
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-16 07:52:42 -0300

    feat: create Generic Error page

--- src/pages/Portal/GenericError/index.tsx
+++ src/pages/Portal/GenericError/index.tsx
@@ -1,5 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+
+import * as S from './styles';
+
 const GenericError = () => {
-  return <div>GenericError</div>;
+  const navigate = useNavigate();
+
+  const isAuthenticated = sessionStorage.getItem('@token');
+
+  const handleBack = () => {
+    navigate(isAuthenticated ? '/' : '/login');
+  };
+
+  return (
+    <S.Container>
+      <div>
+        <S.Heading>Erro Inesperado</S.Heading>
+        <S.Text>
+          Ocorreu um erro inesperado. Por favor, tente novamente ou entre em
+          contato com o suporte.
+        </S.Text>
+        <S.StyledButton onClick={handleBack}>Voltar</S.StyledButton>
+      </div>
+    </S.Container>
+  );
 };
 
 export default GenericError;
