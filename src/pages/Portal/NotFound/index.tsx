3db7d602b459bbdc2534b38414bdc595ace2c1e2
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-16 07:48:33 -0300

    feat: create Not Found page

--- src/pages/Portal/NotFound/index.tsx
+++ src/pages/Portal/NotFound/index.tsx
@@ -1,5 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+
+import * as S from './styles';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const isAuthenticated = sessionStorage.getItem('@token');
+
+  const handleBack = () => {
+    navigate(isAuthenticated ? '/' : '/login');
+  };
+
+  return (
+    <S.Container>
+      <div>
+        <S.Heading>Página Não Encontrada</S.Heading>
+        <S.Text>
+          Desculpe, a página que você está procurando não existe ou foi
+          removida.
+        </S.Text>
+        <S.StyledButton onClick={handleBack}>Voltar</S.StyledButton>
+      </div>
+    </S.Container>
+  );
 };
 
 export default NotFound;
---
+++ src/pages/Portal/NotFound/styles.ts
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+import { Button } from '../../../components/Login/Button';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+  padding: 1.25rem;
+`;
+
+export const Heading = styled.h1`
+  ${({ theme }) => css`
+    font-size: 2rem;
+    font-weight: 700;
+    color: ${theme.colors.secondary};
+    margin-bottom: 1rem;
+  `}
+`;
+
+export const Text = styled.p`
+  ${({ theme }) => css`
+    font-size: 1.25rem;
+    color: ${theme.colors.secondary};
+    margin-bottom: 1rem;
+  `}
+`;
+
+export const StyledButton = styled(Button)`
+  width: 100%;
+  max-width: 300px;
+`;
