df6ba36e11085c0244c00a91bb6e000627610351
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-16 07:08:51 -0300

    feat: add toggle password

--- src/components/Login/Input/index.tsx
+++ src/components/Login/Input/index.tsx
@@ -1,6 +1,7 @@
 import { ErrorMessage, Field } from 'formik';
+import { InputHTMLAttributes, useState } from 'react';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
 
-import { InputHTMLAttributes } from 'react';
 import * as S from './styles';
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -15,9 +16,23 @@ export const Input = ({
   placeholder,
   ...rest
 }: IInputProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => setShowPassword(!showPassword);
+
   return (
     <S.InputGroup>
-      <Field name={name} type={type} placeholder={placeholder} {...rest} />
+      <Field
+        name={name}
+        type={showPassword ? 'text' : type}
+        placeholder={placeholder}
+        {...rest}
+      />
+      {type === 'password' && (
+        <S.PasswordToggle onClick={toggleShowPassword}>
+          {showPassword ? <FiEyeOff /> : <FiEye />}
+        </S.PasswordToggle>
+      )}
       <ErrorMessage name={name} className="error-message" component="div" />
     </S.InputGroup>
   );
--- src/components/Login/Input/styles.ts
+++ src/components/Login/Input/styles.ts
@@ -4,6 +4,7 @@ export const InputGroup = styled.div`
   ${({ theme }) => css`
     position: relative;
     width: 100%;
+    margin-bottom: 20px;
 
     input {
       height: 56px;
@@ -18,7 +19,29 @@ export const InputGroup = styled.div`
     }
 
     .error-message {
+      position: absolute;
+      bottom: -20px; // Posicionar abaixo do input
+      left: 0;
+      width: 100%;
+      text-align: left;
       color: ${theme.colors.danger};
     }
   `}
+`;
+
+export const PasswordToggle = styled.span`
+  position: absolute;
+  top: 50%;
+  right: 15px;
+  transform: translateY(-50%);
+  display: flex;
+  align-items: center;
+  z-index: 10;
+  cursor: pointer;
+  user-select: none;
+
+  svg {
+    height: 1em;
+    width: 1em;
+  }
 `;
