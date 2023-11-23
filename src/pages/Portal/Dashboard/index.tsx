0e06a27032d2c396333fe4c5b0e63b2dfd61e480
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-11-23 09:04:54 -0300

    chore: remove unusal code

--- src/pages/Portal/Dashboard/index.tsx
+++ src/pages/Portal/Dashboard/index.tsx
@@ -1,13 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import TransactionModal from '../../../components/TransactionModal';
-
 const Dashboard = () => {
-  const [isTransactionModalOpen, setTransactionModalOpen] =
-    useState<boolean>(false);
-
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -15,28 +8,9 @@ const Dashboard = () => {
     navigate('/login');
   };
 
-  const handleTransactionConfirm = (transactionData: any) => {
-    console.log(transactionData);
-  };
-
   return (
     <div>
-      <h1>Dashboard</h1>
       <button onClick={handleLogout}>Sair</button>
-
-      <br />
-      <br />
-      <br />
-
-      <button onClick={() => setTransactionModalOpen(true)}>
-        Realizar Transação
-      </button>
-
-      <TransactionModal
-        isOpen={isTransactionModalOpen}
-        onRequestClose={() => setTransactionModalOpen(false)}
-        onConfirm={handleTransactionConfirm}
-      />
     </div>
   );
 };
